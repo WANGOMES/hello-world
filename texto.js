a6f3eb0b093ea94dd350caf8b970f25a1307d9e8
WANGOMES <40408615+WANGOMES@users.noreply.github.com>
2020-02-18 22:01:33 -0300

    Create texto.js

---
+++ texto.js
@@ -0,0 +1,5 @@
+txt = "Aqui você coloca um texto que vai aparecer<br>";
+txt += "na sua página. Não se esqueça que ele aparecerá<br>";
+txt += "extamente onde você o importar";
+
+document.write(txt);
